9b7ef1cf57292968879301481eaab69ed32f6ee9
LemonJust <nadtochi@usc.edu>
2020-02-24 18:36:04 -0800

    remove legacy parts

--- matlab-win/utils/read_tiff3d_movie_timepont.m
+++ matlab-win/utils/read_tiff3d_movie_timepont.m
@@ -1,38 +1,36 @@
 function Img = read_tiff3d_movie_timepont(ProcessFile,zslices,starttime,PixelRegion)
 % starttime: returns one volume at time starttime
 % zslices: number of zslices in one whole vollume
-% PixelRegion: rectangle coordinates - only read stuff inside it 
+% PixelRegion: rectangle coordinates - only read stuff inside it
 
 tiffInfo = imfinfo(ProcessFile);  %# Get the TIFF file information
-Img.nFrame = zslices; 
+Img.nFrame = zslices;
 Img.width = tiffInfo(1).Width;
 Img.height = tiffInfo(1).Height;
 Img.img = uint16(zeros(Img.height,Img.width,Img.nFrame));
-timepoints = 1;
 
 if isempty(zslices)
-     zslices = 1;
+    zslices = 1;
 end
 Img.zslices = zslices;
+
 if isempty(starttime)
-     starttime = 1;
+    starttime = 1;
 end
 
+% get all the region in XY
 if isempty(PixelRegion)
-for t = 1:timepoints
     for z = 1:zslices
-        iFrame = zslices*(t + starttime - 2) + z;
+        iFrame = zslices*(starttime - 1) + z;
         Img.img(:,:,z) = uint16(imread(ProcessFile,...
             'Index',iFrame,'Info',tiffInfo)); % im2double ?
     end
-end
+% get the PixelRegion only
 else
- for t = 1:timepoints
     for z = 1:zslices
-        iFrame = zslices*(t + starttime - 2) + z;
+        iFrame = zslices*(starttime - 1) + z;
         Img.img(:,:,z) = uint16(imread(ProcessFile,...
             'Index',iFrame,'Info',tiffInfo,'PixelRegion',PixelRegion)); % im2double ?
-    end
-end   
+    end 
 end
 end
